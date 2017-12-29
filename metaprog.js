fbdaaa5b32971a413a8027e56ab84eb52f7e103b
Leonardo Val <leonardo.val@creatartis.com>
2017-12-29 18:38:03 -0300

    Simple example + Reads JS source code from the stdin, parses it with esprima and unparses it with escodegen. + Output includes JSON stringification of the AST.

---
+++ metaprog.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+	esprima = require('esprima'),
+	escodegen = require('escodegen');
+
+function main() {
+	var stdin = process.stdin,
+		stdout = process.stdout,
+		source = fs.readFileSync(stdin.fd, { encoding: 'utf8' }),
+		ast = esprima.parse(source); 
+	fs.writeFileSync(stdout.fd, '/* '+ JSON.stringify(ast, null, '\t') +'*/\n');
+	fs.writeFileSync(stdout.fd, escodegen.generate(ast));
+}
+
+main();

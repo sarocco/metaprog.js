e9be27df8ee9100ef1e88bd0e75bc2a6b6731a92
Silvia <silrocco@hotmail.es>
2018-01-04 19:44:16 -0300

    prueba

--- metaprog.js
+++ metaprog.js
@@ -6,7 +6,7 @@ function main() {
 	var stdin = process.stdin,
 		stdout = process.stdout,
 		source = fs.readFileSync(stdin.fd, { encoding: 'utf8' }),
-		ast = esprima.parse(source); 
+		ast = esprima.parse(source);
 	fs.writeFileSync(stdout.fd, '/* '+ JSON.stringify(ast, null, '\t') +'*/\n');
 	fs.writeFileSync(stdout.fd, escodegen.generate(ast));
 }

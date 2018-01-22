ebb1ba567dd2647b8d76770ed3358eddb0e12558
Silvia <silrocco@hotmail.es>
2018-01-22 19:47:58 -0300

    nueva version probando para pasresar solo lo que queda por fuera sin volver a parsear lo de dentro

--- metaprog1.js
+++ metaprog1.js
@@ -14,24 +14,25 @@ function main() {
 
 function searchAstBlock(source) {
 	var inside ="";
+	var cont = 0;
 	//console.log(source);
-	var lowerThan = source.indexOf("[<")+2; //se suman 2 indices dado que el indexOf se para antes de el indice que estoy buscando
+  var lowerThan = source.indexOf("[<")+2; //se suman 2 indices dado que el indexOf se para antes de el indice que estoy buscando
 	var greaterThan = source.lastIndexOf(">]");
 	if (lowerThan == 1 || greaterThan == -1) {//las funciones indexOf y lastIndexOf devuelven -1 cuando no existe el indice a buscar
 		return source;
 	}else{
+    inside = source.split(inside);
 		inside = searchAstBlock(source.substr(lowerThan, greaterThan-lowerThan));//inside es el codigo dentro de [<inside>]
-		console.log(inside)
-	}
+  }
+
 	tree = esprima.parse(inside);
-	var regularExp = /((?:(\[<))|(?:(>\])))/;
+	var regularExp = /((?:(\[>))|(?:(<\])))/;
 	var toArray = source.split(regularExp);//separa el texto en base a la exp regular
 	var string = JSON.stringify(tree, null, '\t');
 
+	var cont =+1
 	var result = toArray[0]+string;
 	return result;
 }
-
-//function searchCodeBlock(source) {}
 
 main();
